f04c10e5206d154eddab41f09e1fce553d58d965
Eric Bach <bach.eric@gmail.com>
2023-11-05 14:17:14 -0700

    Add rules to check sg

--- lambda-shared-eni/checkLambdaSharedEni/app.py
+++ lambda-shared-eni/checkLambdaSharedEni/app.py
@@ -3,14 +3,30 @@ import boto3
 import json
 
 def evaluate_compliance(configuration_item):
-    # Your custom compliance evaluation logic goes here
-    # Example: Check if a Lambda function has a specific tag
-    if "tags" in configuration_item and "application" in configuration_item["tags"]:
+    # Define the desired subnet IDs and security group ID
+    possible_subnet_ids = ["subnet-12345", "subnet-67890"]  # Replace with your desired subnet IDs
+    possible_security_group_ids = ["sg-abcdefg"]  # Replace with your desired security group ID
+    
+    # Check if the Lambda function's subnet IDs and security group match the desired values
+    subnet_ids = configuration_item.get("subnetIds", [])
+    security_group_ids = configuration_item.get("securityGroupIds", [])
+    
+    if set(subnet_ids).issubset(possible_subnet_ids) and len(subnet_ids) == 4 and set(security_group_ids).issubset(possible_security_group_ids) and len(security_group_ids) == 1:
         compliance_status = "COMPLIANT"
     else:
         compliance_status = "NON_COMPLIANT"
     
     return compliance_status
+
+# def evaluate_compliance(configuration_item):
+#     # Your custom compliance evaluation logic goes here
+#     # Example: Check if a Lambda function has a specific tag
+#     if "tags" in configuration_item and "application" in configuration_item["tags"]:
+#         compliance_status = "COMPLIANT"
+#     else:
+#         compliance_status = "NON_COMPLIANT"
+    
+#     return compliance_status
 
 def lambda_handler(event, context):
     print("Event Received:", event)
